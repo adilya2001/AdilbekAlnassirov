60b8a2237e869f5fbd28cc4c059534b15fb98e76
Adilbek Alnassirov <adilya2001@gmail.com>
2019-02-14 14:11:38 +0600

    LAB1

---
+++ lab2/task1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace palindromecheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string s2;
+
+            string s = File.ReadAllText(@"C:\Users\Куралай\source\repos\lab2\task1\text1.txt.txt");
+            char[] ch = s.ToCharArray();
+
+            Array.Reverse(ch);
+            s2 = new string(ch);
+
+            bool b = s.Equals(s2, StringComparison.OrdinalIgnoreCase);
+            if (b == true)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+        }
+    }
+}
---
+++ lab2/task4/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Magic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "Stick.txt";
+            string sourcePath = @"C:\Users\Куралай\source\repos\lab2\task4";
+            string targetPath = @"C:\Users\Куралай\source\repos\lab2\task4\Magic";
+
+            string sourceFile = System.IO.Path.Combine(sourcePath, fileName);
+            string destFile = System.IO.Path.Combine(targetPath, fileName);
+
+            if (!System.IO.Directory.Exists(targetPath))
+            {
+                System.IO.Directory.CreateDirectory(targetPath);
+            }
+
+
+            System.IO.File.Copy(sourceFile, destFile, true);
+
+
+            if (System.IO.Directory.Exists(sourcePath))
+
+            {
+                string[] files = System.IO.Directory.GetFiles(sourcePath);
+
+
+
+                foreach (string s in files)
+                {
+                    fileName = System.IO.Path.GetFileName(s);
+                    destFile = System.IO.Path.Combine(targetPath, fileName);
+                    System.IO.File.Copy(s, destFile, true);
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Source path does not exist!");
+            }
+            if (System.IO.File.Exists(@"C:\Users\Куралай\source\repos\lab2\task4\stick")) ;
+
+            try
+            {
+                System.IO.File.Delete(@"C:\Users\Куралай\source\repos\lab2\task4\stick.txt");
+
+            }
+            catch (System.IO.IOException e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+
+            }
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
